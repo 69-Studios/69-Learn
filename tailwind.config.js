542f48d75f1bf6567f3e22ba1bea22d51f5ed6ff
OceanBreeze <20156521+TheOceanBreeze@users.noreply.github.com>
2022-10-23 13:19:44 -0400

    Starting work on 69Learn

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./content/**/*.{html,js}',
+  './themes/69lt/layouts/**/*.{html,js}'
+],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
